753be674bb50ed37f5a349a47b0d101eabdf995b
tfotr241290 <franco_9024@hotmail.com>
2022-09-24 17:02:00 -0500

    push lesson 2 website

---
+++ scripts/week2Prove.js
@@ -0,0 +1,19 @@
+const one = 5
+const two = 5
+const strOne = 'Hello my name is '
+const strTwo = 'Francisco Franco'
+
+let addition = one + two
+console.log(addition)
+
+let mult = one * two
+console.log(mult)
+
+let subs = one - two
+console.log(subs)
+
+let concat = strOne + strTwo
+console.log(concat)
+
+let comparison = one == two
+console.log(comparison)
